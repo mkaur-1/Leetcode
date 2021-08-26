0f0129a23ded73d1e1e1881990420136f5c09821
Manpreet Kaur <64475346+mkaur-1@users.noreply.github.com>
2021-08-26 11:43:01 +0530

    Update Sum of Square Numbers.cpp

--- Sum of Square Numbers.cpp
+++ Sum of Square Numbers.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+     unordered_map<int,int> m;
+        for(int i=0;i<46340;i++)
+        {
+            m[(i*i)]++;
+            if(m.find(c-(i*i))!=m.end())
+                return true;
+        }
+        return false;
+    }
+};
